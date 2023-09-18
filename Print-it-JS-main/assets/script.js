e9667fc9f7bb562c3fe3d5ffb640a3cb40f3998f
Verbreuk Jordan <jordan-010@hotmail.com>
2023-09-18 19:20:28 +0200

    finish caroussel
    everything works but dots cannot receive class dot_selected

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -16,3 +16,59 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+		let currentImageIndex = 0;
+        const bannerImg = document.querySelector('.banner-img');
+        const arrowLeft = document.querySelector('.arrow-left');
+        const arrowRight = document.querySelector('.arrow-right');
+		const dotsContainer = document.querySelector('.dots')
+
+        function changeImage(index) {
+            bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+        }
+		function changeText(index){
+			textImg.innerHTML = slides[index].tagLine;
+		}
+
+		function createDot(index) {
+			const dot = document.createElement('span');
+			dot.classList.add('dot');
+			dot.addEventListener('click', () => {
+				currentImageIndex = index;
+				changeImage(currentImageIndex);
+			})
+			dotsContainer.appendChild(dot);
+		}
+
+		slides.forEach((slides, index) => {
+			createDot(index);
+		})
+
+		
+		function updateDots(index) {
+			const dots = document.querySelectorAll('.dot');
+				if (i === index) {
+					dots[i].classList.add('dot_selected');
+				} else {
+					dots[i].classList.remove('dot_selected');
+				}
+			}
+
+
+		let textImg = document.querySelector('#banner p');
+
+        arrowLeft.addEventListener('click', () => {
+			console.log("la flèche de gauche fonctionne");
+            currentImageIndex = (currentImageIndex - 1 + slides.length) % slides.length;
+            changeImage(currentImageIndex);
+			changeText(currentImageIndex);
+        });
+
+        arrowRight.addEventListener('click', () => {
+			console.log("la flèche de droite fonctionne");
+            currentImageIndex = (currentImageIndex + 1) % slides.length;
+            changeImage(currentImageIndex);
+			changeText(currentImageIndex);
+        });
+
+	
+		
