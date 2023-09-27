5d0afa5e7549ac42c097e1856c0d6ce265131657
Verbreuk Jordan <jordan-010@hotmail.com>
2023-09-27 14:41:09 +0200

    clean, add alt, fix update dot, make function for all
    clean, add alt, fix update dot, make function for all

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -1,19 +1,23 @@
 const slides = [
 	{
 		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>",
+		"altInfo":"Banner Print-it"
 	},
 	{
 		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+		"altInfo":"Equipe de travail"
 	},
 	{
 		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>",
+		"altInfo":"Nos palette de couleurs"
 	},
 	{
 		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>",
+		"altInfo":"Nos autocollant découpe laser"
 	}
 ]
 		let currentImageIndex = 0;
@@ -24,7 +28,12 @@ const slides = [
 
         function changeImage(index) {
             bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+			// bannerImg.setAttribute(`./assets/images/slideshow/${slides[index].image}`)
         }
+		function changeAlt(index) {
+			// bannerImg.setAttribute("alt", `${slides[index].altInfo}`)
+			bannerImg.alt = `${slides[index].altInfo}`
+		}
 		function changeText(index){
 			textImg.innerHTML = slides[index].tagLine;
 		}
@@ -34,41 +43,49 @@ const slides = [
 			dot.classList.add('dot');
 			dot.addEventListener('click', () => {
 				currentImageIndex = index;
-				changeImage(currentImageIndex);
+				slider()
 			})
 			dotsContainer.appendChild(dot);
 		}
 
-		slides.forEach((slides, index) => {
+		function slider() {
+			changeImage(currentImageIndex);
+			changeText(currentImageIndex);
+			updateDots(currentImageIndex);
+			changeAlt(currentImageIndex);
+		}
+
+		slides.forEach((slide, index) => {
 			createDot(index);
 		})
 
 		
 		function updateDots(index) {
 			const dots = document.querySelectorAll('.dot');
+			slides.forEach((_,i) => {
 				if (i === index) {
 					dots[i].classList.add('dot_selected');
+					
 				} else {
 					dots[i].classList.remove('dot_selected');
-				}
-			}
 
+				}
+			})
+}
 
 		let textImg = document.querySelector('#banner p');
 
         arrowLeft.addEventListener('click', () => {
 			console.log("la flèche de gauche fonctionne");
             currentImageIndex = (currentImageIndex - 1 + slides.length) % slides.length;
-            changeImage(currentImageIndex);
-			changeText(currentImageIndex);
+            slider()
         });
 
         arrowRight.addEventListener('click', () => {
 			console.log("la flèche de droite fonctionne");
             currentImageIndex = (currentImageIndex + 1) % slides.length;
-            changeImage(currentImageIndex);
-			changeText(currentImageIndex);
+			slider()
         });
 
 	
-		
+		slider();
